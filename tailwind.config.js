4b0dafeedec1e4a2be0a12a7b9093e490df2fe6f
lucasptcastro <lucasptcastro@gmail.com>
2024-04-07 16:11:12 -0300

    🎉 init: first commit

---
+++ tailwind.config.js
@@ -0,0 +1,31 @@
+// tailwind.config.js
+module.exports = {
+  content: ["./src/**/*.{ts,tsx}", "./<custom directory>/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        white: "#E1E1E6",
+
+        gray: {
+          200: "#C4C4CC",
+          300: "#8D8D99",
+        },
+
+        green: {
+          500: "#00292E",
+          400: "#28494E",
+          200: "#9FF9CC",
+        },
+        orange: {
+          500: "#F48F56",
+        },
+      },
+      fontFamily: {
+        regular: "Roboto_400Regular",
+        bold: "Roboto_700Bold",
+        medium: "Roboto_500Medium",
+      },
+    },
+  },
+  plugins: [],
+};
